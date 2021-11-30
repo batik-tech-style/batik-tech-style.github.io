4a6451f7acd96064af59f6c02cce4b518e169ad5
batik-tech-style <95166051+batik-tech-style@users.noreply.github.com>
2021-11-30 09:22:33 +0700

    Add js for pop up and auto pause/play

--- app.js
+++ app.js
@@ -41,5 +41,44 @@ window.addEventListener('scroll', () => {
   lastScrollTop = scrollTop;
 });
 
+// Pop up for Youtube Video
+const popup = document.querySelector('.popup-blur');
+const btnClose = document.querySelector('i.bxs-x-circle');
+const boxVideo = document.querySelector('.click-video');
+
+// Auto Pause
+let player;
+function onYouTubePlayerAPIReady() {
+  // create the global player from the specific iframe (#video-yt)
+  player = new YT.Player('video-yt', {
+    events: {
+      // call this function when player is ready to use
+      'onReady': onPlayerReady
+    }
+  });
+}
+
+function onPlayerReady(event) {
+  boxVideo.addEventListener("click", function() {
+    player.playVideo();
+  });
+  btnClose.addEventListener("click", function() {
+    player.pauseVideo();
+  });
+}
+
+// Pop up
+boxVideo.addEventListener('click', () => {
+  popup.classList.add('active');
+})
+btnClose.addEventListener('click', () => {
+  popup.classList.remove('active');
+})
+
+
+const tag = document.createElement('script');
+tag.src = "https://www.youtube.com/player_api";
+const firstScriptTag = document.getElementsByTagName('script')[0];
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 
 
